81654ae5644101c5ad41b3ce1b831a2e49d8efc1
dwaard <daan.de.waard@hz.nl>
2022-11-23 14:39:19 +0100

    Preliminary version

--- soundscape_dirigent.ino
+++ soundscape_dirigent.ino
@@ -2,80 +2,55 @@
 // Als deze macro is gedefinieerd zal de Arduino allerlei debug info naar de 
 // seriele poort sturen.
 #define PRINT
-
-// Het dataformaat voor het liedje
-struct trackItem {
-  unsigned int tick;
-  unsigned int data;
-};
-
-// Include de data van het liedje
-#include "WeWishYouAMerryChristmas_dirigent.h"
+#ifdef PRINT
+  #define SERIAL_BAUD_RATE 115200 // Baud rate voor de USB seriele poort
+#endif
 
 // Pin configuratie:
-#define RESET_PIN 12
-#define PIN_OFFSET 2
-// Pin 13 wordt gebruikt omde ingebouwde LED aan te sturen
-// Voor de tracks worden de pins tussen PIN_OFFSET en RESET_PIN gebruikt
+#define PIN_OFFSET 2 // Eerste bruikbare pin
+#define MAX_PIN 13   // Laatste bruikbare pin
 // Dus bij een PIN_OFFSET van 2: 
-//   track0 -> pin 2; 
-//   track1 -> pin 3; 
-//   etc.
-// Maximaal 10 tracks dan dus
+//   instrument  1 -> pin  2; 
+//   instrument  2 -> pin  3; 
+//   ...
+//   instrument 12 -> pin 13;
+// Maximaal zijn er dus 12 instrumenten beschikbaar
 
-#define MICROSECONDS_PER_TICK (MICROSECONDS_PER_BEAT / TICKS_PER_BEAT)
+#define MIDI_BAUD_RATE 31250 // Standaard baud rate voor MIDI devices
+#define MIDI_CHANNEL 16 // Het afgesproken MIDI channel om MIDI commands te ontvangen
+#define NOTE_ON 144 // Het afgesproken command om een instrument een puls te geven
+#define BASE_NOTE 60 // Noot om instrument 1 aan te sturen. Dus:
+// instrument   1 : 60
+// instrument   2 : 61
+// ...
+// instrument  12 : 71
+
 
 // Pulsduur in microseconde
-#define PULSE_TIME 2500
+#define LOOP_TIME 1000 // vaste tijd in microsec. voor één loop iteratie
+#define PULSE_TIME 5 // Aantal loop iteraties 
 #define MAX_DELAY_MICROSECONDS 16383 // om delayMicroseconds te kunnen gebruiken
 
-// Macro om de trackItem op een gegeven positie in de data array te lezen
-// Dit is nodig omdat de data array in PROGMEM is gedeclareerd
-#define readNextTrackItem(index) (memcpy_P(&currentItem, &track[index], sizeof(trackItem)))
-
 // Globale variabelen
-unsigned int dirigentIndex; // Houdt de array index van het huidige trackItem bij 
-unsigned long tickOffset;   // Onthoudt de tick van het eerste trackItem
-unsigned long startTime; 
-
-trackItem currentItem;      // Het huidige trackItem
+unsigned int pulses[MAX_PIN]; // Item 0 en 1 zullen nooit worden gebruikt
 
 /*
  * De `setup()` functie wordt één keer uitgevoerd na opstarten of een reset
  */
 void setup() {
   #ifdef PRINT
-    Serial.begin(115200);
+    Serial.begin(SERIAL_BAUD_RATE);
     Serial.println("Starting the conductor in debug mode");
   #endif
-
+  Serial1.begin(MIDI_BAUD_RATE);
   // Initialiseer de pins
-  pinMode(RESET_PIN, OUTPUT);
   pinMode(LED_BUILTIN, OUTPUT);
-  for (int n = 0; n < AMOUNT_OF_INSTRUMENTS; n++) {
+  for (int n = PIN_OFFSET; n < MAX_PIN; n++) {
     pinMode(n + PIN_OFFSET, OUTPUT);
   }
-
-  // Lees het eerste item uit de dirigent array
-  dirigentIndex = 0;
-  readNextTrackItem(dirigentIndex++);
-
-  // Initialiseer de overige variabelen
-  tickOffset = currentItem.tick;
-  #ifdef PRINT
-    Serial.print("Microseconds per tick: ");
-    Serial.println(MICROSECONDS_PER_TICK);
-    Serial.print("Time offset: ");
-    Serial.println(tickOffset);
-  #endif
-
-  // Stuur de reset puls naar de instrumenten
-  digitalWrite(RESET_PIN, 1);
-  delayMicroseconds(PULSE_TIME);
-  digitalWrite(RESET_PIN, 0);
-  // Wacht een seconde voor het liedje gaat afspelen
-  delay(5000);  
-  startTime = micros();
+  for (int n = PIN_OFFSET; n < MAX_PIN; n++) {
+    pulses[n] = 0;
+  }
 } // Einde van de `setup()` functie
 
 
@@ -84,68 +59,39 @@ void setup() {
  * uitgezet of gereset.
  */
 void loop() {
-  if (dirigentIndex <= DIRIGENTSIZE) {
-    // Stuur de output pins aan
-    #ifdef PRINT
-      Serial.print(dirigentIndex);
-      Serial.print("/");
-      Serial.print(DIRIGENTSIZE);
-      Serial.print(": ");
-      print_binary(currentItem.data, AMOUNT_OF_INSTRUMENTS);
-      Serial.print(" ");
-    #endif
-    for (int n = 0; n < AMOUNT_OF_INSTRUMENTS; n++) {
-      digitalWrite(LED_BUILTIN, HIGH);
-      digitalWrite(n + PIN_OFFSET, currentItem.data & (1 << n));
-    }
-    delayMicroseconds(PULSE_TIME);
-    for (int n = 0; n < AMOUNT_OF_INSTRUMENTS; n++) {
-      digitalWrite(n + PIN_OFFSET, LOW);
-      digitalWrite(LED_BUILTIN, LOW); 
-    }
-
-    if (dirigentIndex < DIRIGENTSIZE) {
-      // Lees het volgende item uit de dirigent array
-      readNextTrackItem(dirigentIndex++);
-
-      // Bepaal hoe lang er gewacht moet worden tot het volgende event
-      unsigned long timeToNextEvent = (currentItem.tick - tickOffset) * MICROSECONDS_PER_TICK;
-      unsigned long currentTime = micros() - startTime;
-      unsigned long delayTime = 0;
-      if (currentTime < timeToNextEvent) {
-        delayTime = timeToNextEvent - currentTime;
+  // Meet de process tijd
+  unsigned long start = micros();
+  // Verwerk de MIDI commands
+  if (Serial1.available() > 2) { // Als er minstens 3 bytes beschikbaar zijn
+    byte command = Serial1.read();
+    byte note = Serial1.read();
+    byte velocity = Serial1.read();
+    byte channel = command && 0b00001111;
+    if (channel == (MIDI_CHANNEL - 1)) {
+      command = command && 0b11110000;
+      if (command == NOTE_ON) {
+        pulses[PIN_OFFSET + note - BASE_NOTE] = PULSE_TIME;
       }
-
-      // Wacht op het volgende event
       #ifdef PRINT
-        unsigned long temp = micros();  
-        Serial.print("Delaying (us): ");
-        Serial.println(delayTime);
-        unsigned long delta = micros() - temp;
-        if (delta < delayTime) {
-          delayTime -= delta;
-        } else {
-          delayTime = 0;
-        }
+        Serial.print("Instrument ");
+        Serial.println(note - BASE_NOTE + 1);
       #endif
-      if (delayTime <= MAX_DELAY_MICROSECONDS) {
-        delayMicroseconds(delayTime);
-      } else {
-        delay(delayTime / 1000);
-      }
-    }      
+    }
+  }
+  // De outputs aansturen
+  for (int n = PIN_OFFSET; n < MAX_PIN; n++) {
+    if (pulses[n] > 0) {
+      digitalWrite(n, HIGH);
+      pulses[n]--;
+    } else {
+      digitalWrite(n, LOW);
+    }
+  }
+  // Delay voor de volgende loop
+  unsigned long delayTime = LOOP_TIME - (micros() - start);
+  if (delayTime <= MAX_DELAY_MICROSECONDS) {
+    delayMicroseconds(delayTime);
+  } else {
+    delay(delayTime / 1000);
   }
 }// Einde van de `loop()` functie
-
-#ifdef PRINT
-void print_binary(int number, byte Length) {
-  static int Bits;
-  if (number) { //The remaining bits have a value greater than zero continue
-    Bits++; // Count the number of bits so we know how many leading zeros to print first
-    print_binary(number >> 1, Length); // Remove a bit and do a recursive call this function.
-    if (Bits) for (byte x = (Length - Bits); x; x--)Serial.write('0'); // Add the leading zeros
-    Bits = 0; // clear no need for this any more
-    Serial.write((number & 1) ? '1' : '0'); // print the bits in reverse order as we depart the recursive function
-  }
-}
-#endif
