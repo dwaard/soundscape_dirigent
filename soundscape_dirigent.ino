998b7bc709f3c2c7e3cd61fc5555c29b5231b05f
dwaard <daan.de.waard@hz.nl>
2022-12-05 12:04:06 +0100

    Make notes to pins configurable

--- soundscape_dirigent.ino
+++ soundscape_dirigent.ino
@@ -6,16 +6,6 @@
   #define SERIAL_BAUD_RATE 115200 // Baud rate voor de USB seriele poort
 #endif
 
-// Pin configuratie:
-#define PIN_OFFSET 2 // Eerste bruikbare pin
-#define MAX_PIN 13   // Laatste bruikbare pin
-// Dus bij een PIN_OFFSET van 2: 
-//   instrument  1 -> pin  2; 
-//   instrument  2 -> pin  3; 
-//   ...
-//   instrument 12 -> pin 13;
-// Maximaal zijn er dus 12 instrumenten beschikbaar
-
 // MIDI settings
 #define MIDI_BAUD_RATE 31250 // Standaard baud rate voor MIDI devices
 #define MIDI_CHANNEL 9 // Het afgesproken MIDI channel om MIDI commands te ontvangen (0..15)
@@ -30,24 +20,35 @@
 #define PITCH_BEND            0b11100000 // 224
 #define NON_MUSICAL           0b11110000 // 240
 
-#define BASE_NOTE 31 // Noot om instrument 1 aan te sturen. Dus bv:
-// instrument   1 : 60
-// instrument   2 : 61
-// ...
-// instrument  12 : 71
-
-// Timing settings
-#define LOOP_TIME 1000 // vaste tijd in microsec. voor één loop iteratie
-#define PULSE_TIME 25 // Aantal loop iteraties voor een puls 
-#define MAX_DELAY_MICROSECONDS 16383 // om delayMicroseconds te kunnen gebruiken
-
 typedef struct MIDI_Command {
   byte channel;
   byte type;
   byte params[2];
 };
 
-// Globale variabelen
+// Timing settings
+#define LOOP_TIME 1000 // vaste tijd in microsec. voor één loop iteratie
+#define PULSE_TIME 50 // Aantal loop iteraties voor een puls 
+#define MAX_DELAY_MICROSECONDS 16383 // om delayMicroseconds te kunnen gebruiken
+
+// Pin configuratie:
+#define MIN_PIN 2  // Eerste bruikbare pin
+#define MAX_PIN 13 // Laatste bruikbare pin
+
+// Macro dat bepaalt of een ontvangen byte een MIDI command is
+#define isMidiCommand(cmd) (cmd > 127)
+// Macro dat bepaalt of een MIDI command een 2e parameter heeft
+#define hasSecondParam(cmd) (cmd != PATCH_CHANGE && cmd != CHANNEL_PRESSURE)
+
+// De mapping van notes naar pins. De array is 128 elementen, corresponderedend met
+// het aantal mogelijke noten. De inhoud van elk element is een pin nummer. Als de noot
+// Niet wordt gebruikt bevat deze de waarde 0.
+// In de `setup()` functie moet de mapping worden ingesteld.
+byte notes[128] = {0}; 
+
+// De gewenste toestand van de output pins. De index van de array komt overeen met 
+// een output pin nummer en de waarde een getal >=0. Als het getal >0 wordt die pin
+// met een `HIGH`
 unsigned int pulses[MAX_PIN] = {0}; // Item 0 en 1 zullen nooit worden gebruikt
 
 /*
@@ -59,13 +60,18 @@ void setup() {
     Serial.println("Starting the conductor in debug mode");
   #endif
   Serial1.begin(MIDI_BAUD_RATE);
-  // Initialiseer de pins
-  pinMode(LED_BUILTIN, OUTPUT);
-  for (int n = PIN_OFFSET; n < MAX_PIN; n++) {
-    pinMode(n + PIN_OFFSET, OUTPUT);
-  }
-  for (int n = PIN_OFFSET; n < MAX_PIN; n++) {
-    pulses[n] = 0;
+  // Koppel de gebruikte noten aan de output pins mbv de notes array  
+  // syntax: notes[note] = pin;
+  notes[36] = 2;
+  notes[37] = 3;
+  notes[38] = 4;
+  notes[42] = 5;
+  notes[44] = 6;
+  notes[69] = 7;
+  // ...
+  // Initialiseer alle beschikbare pins maar als output pins
+  for (int n = MIN_PIN; n <= MAX_PIN; n++) {
+    pinMode(n, OUTPUT);
   }
 } // Einde van de `setup()` functie
 
@@ -79,17 +85,25 @@ void loop() {
   // Ontvang en verwerk MIDI commands
   if (Serial1.available()) {
     MIDI_Command cmd;
-
+    // Lees alle databytes totdat een MIDI command is ontvangen
     byte recieved = Serial1.read();
-    if (recieved > 127) {
+    if (isMidiCommand(recieved)) {
       cmd.type = recieved & 0b11110000;
       cmd.channel = recieved & 0b00001111;
       cmd.params[0] = Serial1.read();
-      if (cmd.type != PATCH_CHANGE && cmd.type != CHANNEL_PRESSURE ) {
+      if (hasSecondParam(cmd.type)) {
         cmd.params[1] = Serial1.read();
-      }      
-      #ifdef PRINT
-        if (cmd.type != NON_MUSICAL) { // Filter dit command (komt vaak voor)
+      }
+      // Als er een afgesproken command op het afgesproken channel is ontvangen...
+      if (cmd.channel == MIDI_CHANNEL && (cmd.type == NOTE_ON || cmd.type == NOTE_OFF)) {
+        // Lees de aan te sturen pin nummer uit de notes array
+        int index = notes[cmd.params[0]];
+        // Zet die pin dan aan door de notes array te vullen
+        if (index >= MIN_PIN) {
+          pulses[index] = PULSE_TIME;
+        } 
+        #ifdef PRINT
+        else {
           Serial.print("Channel ");
           Serial.print(cmd.channel);
           Serial.print("; Type ");
@@ -97,22 +111,13 @@ void loop() {
           Serial.print("; Note ");
           Serial.println(cmd.params[0]);
         }
-      #endif
-      if (cmd.channel == (MIDI_CHANNEL) && cmd.type == NOTE_ON) {
-        int index = PIN_OFFSET + cmd.params[0] - BASE_NOTE;
-        #ifdef PRINT
-        Serial.print("PULSE: ");
-        Serial.println(index);
         #endif
-        if (index < MAX_PIN) {
-          pulses[index] = PULSE_TIME;
-        }
-      }
+      } 
     }
   }
 
-  // De outputs aansturen
-  for (int n = PIN_OFFSET; n < MAX_PIN; n++) {
+  // De outputs aansturen dmv de notes array
+  for (int n = MIN_PIN; n < MAX_PIN; n++) {
     if (pulses[n] > 0) {
       digitalWrite(n, HIGH);
       pulses[n]--;
